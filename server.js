f50a1b28863e8babd9340488a715bb2a6f92249e
giveonaldo <giveonaldo@gmail.com>
2024-12-02 22:41:39 +0700

    setup initial project views with ejs

---
+++ server.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const express = require('express');
+const expressLayout = require("express-ejs-layouts");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Layout
+app.use(expressLayout);
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.set("layout", "./layouts/main");
+app.use(express.urlencoded({ extended: true }));
+
+// Route Home
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: "HomePage"
+    });
+});
+
+// Route Login
+app.get('/login', (req, res) => {
+    res.render('login', {
+        title: "Login"
+    });
+});
+
+// Route Movies
+app.get('/movies', (req, res) => {
+    res.render('movies', {
+        title: "Movie"
+    });
+});
+
+// Route Register
+app.get('/register', (req, res) => {
+    res.render('register', {
+        title: "Register"
+    });
+});
+
+// Route Series
+app.get('/series', (req, res) => {
+    res.render('series', {
+        title: "Series"
+    });
+});
+
+// Starting app
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+})
