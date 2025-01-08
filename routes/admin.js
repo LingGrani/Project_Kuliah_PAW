c5e8f809225d026f0d6e765c0c420ed3c00590d7
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2025-01-08 19:52:02 +0700

    fix admin.ejs

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,6 @@ require('dotenv')
 const express = require('express')
 const router = express.Router();
 
-router.use((req, res, next) => {
-    req.app.set('layout', 'layouts/admin-layout');
-    next();
-});
-
 router.get('/', (req, res) => {
     return res.render('admin/index');
 });
