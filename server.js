507dfb2bd3d4634f33afc05b45d4b0a59765a1e6
farhan <farhanvirenze18@gmail.com>
2025-01-09 07:34:30 +0700

    update route

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use('/admin', require('./routes/admin'));
 
 // Route Home
 app.get('/', (req, res) => {
-    res.render('index', {
+    res.render('register', {
         title: "HomePage",
-        isNavbarPage: false,
+        isNavbarPage: true,
         isFooterPage: false,
     });
 });
@@ -43,7 +43,7 @@ app.post('/login', (req, res) => {
     // Logika autentikasi sederhana
     if (username === 'user' && password === 'password') {
         // Login berhasil, arahkan ke halaman index
-        res.redirect('/');
+        res.redirect('/index');
     } else {
         // Login gagal, kembalikan ke halaman login dengan pesan kesalahan
         res.status(401).send('Invalid username or password');
@@ -78,10 +78,10 @@ app.get('/watchlist', (req, res) => {
 });
 
 // Route Register
-app.get('/register', (req, res) => {
-    res.render('register', {
-        title: "Register",
-        isNavbarPage: true,
+app.get('/index', (req, res) => {
+    res.render('index', {
+        title: "Index",
+        isNavbarPage: false,
         isFooterPage: false,
     });
 });
